e0924988e07082836372594a927ed4c8806216fc
AzureAD\MingLiangChen <cmliang@cn.ibm.com>
2018-11-30 12:27:38 +0800

    update

--- src/main/java/com/citylife/devops/turbine/TurbineServerApplication.java
+++ src/main/java/com/citylife/devops/turbine/TurbineServerApplication.java
@@ -2,21 +2,21 @@ package com.citylife.devops.turbine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
+import org.springframework.cloud.netflix.turbine.stream.TurbineStreamAutoConfiguration;
+//import org.springframework.kafka.annotation.EnableKafka;
 
 /**
- * 注意:
- * TurbineStream工程无法和HystrixDashboard工程共存必须分开
- * 否则会引起Dashboard页面无法显示
+ * 注意: TurbineStream工程无法和HystrixDashboard工程共存必须分开 否则会引起Dashboard页面无法显示
  *
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = { TurbineStreamAutoConfiguration.class })
 @EnableTurbineStream
-@EnableDiscoveryClient
+@EnableEurekaClient
 public class TurbineServerApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(TurbineServerApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(TurbineServerApplication.class, args);
+	}
 }
---
+++ src/main/java/com/citylife/devops/turbine/TurbineStreamAutoConfigurationWrapper.java
@@ -0,0 +1,9 @@
+package com.citylife.devops.turbine;
+
+import org.springframework.cloud.netflix.turbine.stream.TurbineStreamAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TurbineStreamAutoConfigurationWrapper extends TurbineStreamAutoConfiguration {
+
+}
